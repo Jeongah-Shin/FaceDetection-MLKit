490d14cf8a0b063041d41119b06eeed9ec2ef936
Jeongah-Shin <jeongah.h.shin@gmail.com>
2020-07-14 10:55:02 +0900

    Basic structure for inferencing MLKit face detection model; the issue about androidx migration remains

--- basis/app/src/main/java/com/jeongari/facedetection_mlkit/CameraActivity.kt
+++ basis/app/src/main/java/com/jeongari/facedetection_mlkit/CameraActivity.kt
@@ -2,6 +2,7 @@ package com.jeongari.facedetection_mlkit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jeongari.facedetection_mlkit.camera.Camera2BasicFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class CameraActivity : AppCompatActivity() {
@@ -9,9 +10,12 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        if (null == savedInstanceState) {
+            fragmentManager
+                .beginTransaction()
+                .replace(R.id.container, Camera2BasicFragment.newInstance())
+                .commit()
+        }
     }
 
     /**
